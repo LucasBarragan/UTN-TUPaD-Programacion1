e722f3704bdeb857548b47ea1adc1f01a712280e
LucasBarragan <lucasbarragan000@gmail.com>
2026-04-08 19:41:49 -0300

    Subo práctica unidad 1
    Subo la práctica de la unidad 1 para completar la actividad de la Unidad 4: Trabajo Colaborativo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # UTN-TUPaD-Programacion1
+
+# Agrego la actividad de la unidad uno para completar la práctica de la unidad 4
---
+++ trabajo_colaborativo.py
@@ -0,0 +1,88 @@
+#1
+
+#print("Hola Mundo")
+
+#2
+
+#nombre = input("Ingresa tu nombre: ")
+#print(f"Hola {nombre}!")
+
+#3
+
+#nombre = input("Ingresa tu nombre: ")
+#apellido = input("Ingresa tu apellido: ")
+#edad = input("Ingresa tu edad: ")
+#lugarDeResidencia = input("Ingresa tu lugar de residencia: ")
+
+#print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {lugarDeResidencia}")
+
+#4
+
+#radio = float(input("Ingresa el radio del circulo que deseas para calcular el perimetro: "))
+#area = 3.14 * radio * radio
+#perimetro = 2 * 3.14 * radio
+
+#print(f"El area es {area} y el perimetro es {perimetro} ")
+
+#5
+
+#segundos = float(input("Ingresa la cantidad de segundos que deseas transformar a horas: "))
+#unaHora = 3600
+#resultado = segundos / unaHora  
+
+#print(f"{segundos} equivalen a {resultado} horas.")
+
+#6
+
+#numero = int(input("Ingresa un numero para obtener la tabla de multiplicar de dicho numero: "))
+#print(numero * 1)
+#print(numero * 2)
+#print(numero * 3)
+#print(numero * 4)
+#print(numero * 5)
+#print(numero * 6)
+#print(numero * 7)
+#print(numero * 8)
+#print(numero * 9)
+#print(numero * 10)
+
+#7
+
+#num1 = int(input("Ingresa el primer numero: "))
+#num2 = int(input("Ingresa el segundo numero: "))
+
+#suma = num1 + num2
+#mult = num1 * num2
+#div = num1 / num2
+#resta = num1 - num2
+
+#print(f"{suma}")
+#print(f"{mult}")
+#print(f"{div}")
+#print(f"{resta}")
+
+#8
+
+#peso = float(input("Ingresa tu peso en kg: "))
+#altura = float(input("Ingresa tu altura en metros: "))
+#imc = peso / (altura **2)
+
+#print(f"Tu IMC es: {imc}")
+
+#9
+
+#celsius = float(input("Ingresa la temperatura en grados Celsius: "))
+#resultado = (9/5) * celsius + 32
+
+#print(f"{celsius} grados Celsius son {resultado} grados Fahrenheit")
+
+#10
+
+#n1 = float(input("Ingresa el primer numero: "))
+#n2 = float(input("Ingresa el segundo numero: "))
+#n3 = float(input("Ingresa el tercer numero: "))
+#promedio = (n1 + n2 + n3) / 3
+
+#print(f"El promedio entre los 3 numeros elegidos es: {promedio}")
+
+#Lucas Gabriel Barragan
